63b8708c4d2d069871bfa2db535fd73c3e6585da
maddrifter <sirius.dev719@gmail.com>
2017-02-01 03:36:18 +0100

    ** update theme.  bug fixing

---
+++ assets/js/application.js
@@ -0,0 +1,14 @@
+// // Save search term
+//     $("#nav-search").keydown(function(e) {
+//         if(e.which == 13) {
+//             var searchVal = $('#nav-search').val();
+//             sessionStorage.setItem("searchVal", searchVal);
+//         }
+//         index.add(parsedData);
+//     blogData.push(parsedData);
+//   };
+//   $('#search-field').keyup();    // the actual line you're adding
+//     });
+
+//     // Write search term to search form
+//     $('#search-field').val(sessionStorage.getItem("searchVal"));
